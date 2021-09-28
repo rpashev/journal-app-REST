3c5eda6f600311edfbbb14a8ed5fb14bc3634b4b
rpashev <rossen1991@gmail.com>
2021-09-28 13:35:19 +0300

    added an error model

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const journalRoutes = require("./routes/journal-routes");
 
 const app = express();
 
-app.use(journalRoutes);
+app.use("/journals", journalRoutes);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "Uknown error occurred!" });
+});
 // app.use(bodyParser.urlencoded);
 
 app.listen(5000, () => console.log("listening on port 5000..."));
--- routes/journal-routes.js
+++ routes/journal-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 const journals = require("../DUMMY_DATA");
 
@@ -7,6 +9,19 @@ router.get("/", (req, res, next) => {
   console.log(journals);
   res.json(journals);
   next();
+});
+
+router.get("/:journalID", (req, res, next) => {
+  const journalID = req.params.journalID;
+  const journal = journals.find((journal) => journal.id === journalID);
+  if (!journal) {
+    const error = new HttpError(
+      "Could not find a journal for the provided ID",
+      404
+    );
+    return next(error);
+  }
+  res.json(journal);
 });
 
 module.exports = router;
