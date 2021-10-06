cf18864dda132e53c0ece31bbb59b41e0cf65709
rpashev <rossen1991@gmail.com>
2021-10-06 17:43:27 +0300

    get journal controller implemented with mongo

--- controllers/journal-controllers.js
+++ controllers/journal-controllers.js
@@ -6,20 +6,43 @@ const User = require("../models/user");
 const userID = "6159cd86b1d83b59a675123f";
 
 const getAllJournals = (req, res, next) => {
+  //for displaying list of journals with names/description so populate needed
   res.json(journals);
   next();
 };
 
-const getJournal = (req, res, next) => {
+const getJournal = async (req, res, next) => {
+  //for displaying all the entries, needs to check the creator of the journal
+
   const journalID = req.params.journalID;
-  const journal = journals.find((journal) => journal.id === journalID);
-  if (!journal) {
+
+  if (
+    !mongoose.Types.ObjectId.isValid(userID) ||
+    !mongoose.Types.ObjectId.isValid(journalID)
+  ) {
+    const error = new HttpError("Invalid credentials or journal ID!", 400);
+    return next(error);
+  }
+  let journal;
+  let data;
+
+  try {
+    data = await Journal.find({ _id: journalID, creatorID: userID });
+    if (data.length === 0) {
+      const error = new HttpError(
+        "Could not find a journal for the provided ID",
+        404
+      );
+      return next(error);
+    }
+  } catch (err) {
     const error = new HttpError(
-      "Could not find a journal for the provided ID",
-      404
+      "Something went wrong, please try again later!",
+      500
     );
     return next(error);
   }
+  journal = data[0];
   res.json(journal);
 };
 
@@ -28,6 +51,14 @@ const createJournal = async (req, res, next) => {
   if (!journalName) {
     const error = new HttpError(
       "Could not craete a journal as journal name is required!",
+      400
+    );
+    return next(error);
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(userID)) {
+    const error = new HttpError(
+      "Invalid credentials, could not create journal!",
       400
     );
     return next(error);
@@ -55,7 +86,7 @@ const createJournal = async (req, res, next) => {
     const error = new HttpError("No such user!", 404);
     return next(error);
   }
-  
+
   if (user.journals.length > 0) {
     const journal = user.journals.find(
       (journal) => journal.journalName === journalName
@@ -87,6 +118,7 @@ const createJournal = async (req, res, next) => {
 };
 
 const deleteJournal = (req, res, next) => {
+  //needs to delete from journals + user
   const journalID = req.params.journalID;
 
   if (!journalID) {
