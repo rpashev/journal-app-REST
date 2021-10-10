711183613462cb929599830662a13b21446e01fd
rpashev <rossen1991@gmail.com>
2021-10-10 17:07:44 +0300

    adding a get single entry controller

--- controllers/entry-controllers.js
+++ controllers/entry-controllers.js
@@ -162,9 +162,47 @@ const deleteEntry = async (req, res, next) => {
   res.status = 200;
   res.json(journal.entries);
 };
+const getEntry = async (req, res, next) => {
+  const { journalID, entryID } = req.params;
+  const userID = req.userData.userId;
+  console.log(userID, journalID, entryID)
+
+  if (!mongoose.Types.ObjectId.isValid(entryID)) {
+    const error = new HttpError("Invalid entry id!");
+    return next(error);
+  }
+
+  let journal;
+
+  try {
+    const result = await getJournalService(userID, journalID);
+    if (result.code) {
+      return next(result);
+    } else {
+      journal = result;
+    }
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, please try again later!",
+      500
+    );
+    return next(error);
+  }
+  const entry = journal.entries.find((entry) => entry.id === entryID);
+  if (!entry) {
+    const error = new HttpError(
+      "Could not find an entry for the provided ID!",
+      404
+    );
+    return next(error);
+  }
+  res.status = 200;
+  res.json(entry);
+};
 
 exports.entryControllers = {
   createEntry,
   updateEntry,
   deleteEntry,
+  getEntry,
 };
--- routes/entry-routes.js
+++ routes/entry-routes.js
@@ -5,6 +5,7 @@ const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
+router.get("/journals/:journalID/:entryID", entryControllers.getEntry);
 router.post("/journals/:journalID/create-entry", entryControllers.createEntry);
 router.patch("/journals/:journalID/:entryID", entryControllers.updateEntry);
 router.delete("/journals/:journalID/:entryID", entryControllers.deleteEntry);
