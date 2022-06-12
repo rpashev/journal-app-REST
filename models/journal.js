bc66c0e64563943855b24c2f17754474b41e9c0a
rpashev <rossen1991@gmail.com>
2022-06-12 15:02:43 +0300

    mongoose schema errors

--- models/journal.js
+++ models/journal.js
@@ -2,13 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const journalSchema = new Schema({
-  journalName: { type: String, required: true, maxLenght: 40 },
+  journalName: {
+    type: String,
+    required: [true, "Journal name is required"],
+    maxLenght: [40, "Journal name can't be more than 40 characters"],
+  },
   description: String,
-  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Creator ID is required"],
+    ref: "User",
+  },
   entries: [
     {
       title: String,
-      body: { type: String, required: true },
+      body: { type: String, required: [true, "Entry body is required"] },
       date: { type: Date, default: new Date() },
     },
   ],
--- models/user.js
+++ models/user.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
+  firstName: { type: String, required: [true, "First name is required"] },
+  lastName: { type: String, required: [true, "Last name is required"] },
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+  },
   termsAgreement: { type: Boolean, default: false },
   updatesAgreement: { type: Boolean, default: false },
   journals: [{ type: mongoose.Types.ObjectId, ref: "Journal" }],
